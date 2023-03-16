934421a6252cf13c04c1e7f97d799a0097ee4cdc
abrarzahid050 <abrar.zahid050@gmail.com>
2023-03-17 04:49:04 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddPostForm from "./views/form/AddPostForm";
+import PostsList from "./views/posts/PostsList";
+
+function App() {
+  return (
+    <>
+      <AddPostForm />
+      <PostsList />
+    </>
+  );
+}
+
+export default App;
