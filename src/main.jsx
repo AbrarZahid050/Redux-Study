47165af4b96e2ca603357632bbe62710fa3e0acf
abrarzahid050 <abrar.zahid050@gmail.com>
2023-03-17 17:49:48 +0500

    createAysnThunk done!

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { fetchUsers } from "./redux/userSlice";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
 );
