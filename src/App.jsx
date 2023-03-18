71ab5dc424b6501dfa167f07cab1b69faf9d62e0
abrarzahid050 <abrar.zahid050@gmail.com>
2023-03-18 21:15:44 +0500

    rtk query!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import AddPostForm from "./views/form/AddPostForm";
-import PostsList from "./views/posts/PostsList";
+import { useGetTodosQuery } from "./redux/apiSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
+const App = () => {
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = todos.map((todo) => {
+      return (
+        <article key={todo.id}>
+          <div className="todo">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              id={todo.id}
+              onChange={() => {}}
+            />
+            <label htmlFor={todo.id}>{todo.title}</label>
+          </div>
+          <button className="trash" onClick={() => {}}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
+
   return (
-    <>
-      <AddPostForm />
-      <PostsList />
-    </>
+    <main>
+      <h1>Todo List</h1>
+      {content}
+    </main>
   );
-}
+};
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postSlice from "./postSlice";
-import userSlice from "./userSlice";
+import { pokemonApi } from "./apiSlice";
+// import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   reducer: {
-    posts: postSlice,
-    users: userSlice,
+    // Add the generated reducer as a specific top-level slice
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+// setupListeners(store.dispatch)
